c5d655e632b2a1bb72b539788b6677362c1159b9
KeeganHeitman <74746845+KeeganHeitman@users.noreply.github.com>
2021-10-27 12:14:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ search=https://www.google.com/search?q=apple+stock
 with open("index.html") as fp:
   soup=BeautifulSoup(fp,'lmxl')
 soup=BeautifulSoup("search","lmxl")
+what_i_want=<span jsname="vWLAgc" class="IsqQVc NprOob XcVN5d">149.17</span>
