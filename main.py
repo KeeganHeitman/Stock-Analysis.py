b4a57a7cabafd429b3f7678983c27528109d78a3
KeeganHeitman <74746845+KeeganHeitman@users.noreply.github.com>
2021-10-27 12:10:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import BeautifulSoup,tkinter,MediaWiki,lmxl
+search=https://www.google.com/search?q=apple+stock
 with open("index.html") as fp:
   soup=BeautifulSoup(fp,'lmxl')
-soup=BeautifulSoup("<html>a web page<html>","lmxl")
+soup=BeautifulSoup("search","lmxl")
